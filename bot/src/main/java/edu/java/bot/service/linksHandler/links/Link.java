19c34650b1a3f4f6bb8245efd4a940f6385021a3
Ushenko_IAroslav <368952@niuitmo.ru>
2024-02-25 23:43:40 +0300

    stl below mvp

--- bot/src/main/java/edu/java/bot/service/linksHandler/links/Link.java
+++ bot/src/main/java/edu/java/bot/service/linksHandler/links/Link.java
@@ -1,3 +1,6 @@
 package edu.java.bot.service.linksHandler.links;
 
-public interface Link {}
+public interface Link {
+    public String getHost();
+    public String getPath();
+}
--- bot/src/main/java/edu/java/bot/service/linksHandler/links/StackOverFlowLink.java
+++ bot/src/main/java/edu/java/bot/service/linksHandler/links/StackOverFlowLink.java
@@ -1,3 +1,13 @@
 package edu.java.bot.service.linksHandler.links;
 
-public record StackOverFlowLink(String host, String path) implements Link {}
+public record StackOverFlowLink(String host, String path) implements Link {
+    @Override
+    public String getHost() {
+        return host;
+    }
+
+    @Override
+    public String getPath() {
+        return path;
+    }
+}
