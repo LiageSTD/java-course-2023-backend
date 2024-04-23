115e3da38ed35498bacf5b05f353a595e0f874b4
LiageSTD <368952@edu.itmo.ru>
2024-04-23 22:42:38 +0300

    migrations fixed

--- migrations/tables/link.sql
+++ migrations/tables/link.sql
@@ -1,21 +1,10 @@
 --liquibase formatted sql
 
 --changeset liage:02
-CREATE TABLE IF NOT EXISTS links
+CREATE TABLE IF NOT EXISTS link
 (
-    id
-    BIGSERIAL
-    PRIMARY
-    KEY,
-    url
-    TEXT
-    NOT
-    NULL
-    UNIQUE,
-    updated_at
-    TIMESTAMPTZ
-    NOT
-    NULL,
-    unable_to_update
-    BOOLEAN
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    url TEXT NOT NULL UNIQUE,
+    updated_at TIMESTAMPTZ NOT NULL,
+    unable_to_update BOOLEAN
 );
