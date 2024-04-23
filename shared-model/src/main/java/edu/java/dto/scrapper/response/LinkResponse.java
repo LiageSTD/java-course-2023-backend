fd1d417f2e4c17f8c95bd3a26aac0164c9468b72
Ushenko_IAroslav <368952@niuitmo.ru>
2024-04-23 15:16:45 +0300

    HW URI + RCode Fixed

--- shared-model/src/main/java/edu/java/dto/scrapper/response/LinkResponse.java
+++ shared-model/src/main/java/edu/java/dto/scrapper/response/LinkResponse.java
@@ -2,10 +2,11 @@ package edu.java.dto.scrapper.response;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import java.net.URI;
 
 @Getter
 @AllArgsConstructor
 public class LinkResponse {
     int id;
-    String url;
+    URI uri;
 }
