97a4c76e83663c83694b06a4c120085b65bd5112
LiageSTD <368952@edu.itmo.ru>
2024-04-28 18:49:29 +0300

    HW4 PgTests Fixed

--- migrations/tables/user_link.sql
+++ migrations/tables/user_link.sql
@@ -1,10 +1,10 @@
 --liquibase formatted sql
 
 --changeset liage:03
-CREATE TABLE IF NOT EXISTS users_link
+CREATE TABLE IF NOT EXISTS chat_link
 (
     chat_id BIGINT REFERENCES chat (id),
 
     link_id BIGINT REFERENCES link (id),
-    CONSTRAINT users_links_pk PRIMARY KEY (chat_id, link_id)
+    CONSTRAINT chat_link_pk PRIMARY KEY (chat_id, link_id)
 );
